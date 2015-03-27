c8b856af959a1d5f5331031a5afb8f44d33c41bd
Yoshgunn <yoshgunn@gmail.com>
2015-03-27 18:08:41 -0400

    Player Shooting - Part 4
    Now you can die and respawn at the top of the level

--- Character_Controller.js
+++ Character_Controller.js
@@ -59,6 +59,44 @@ pc.script.create('Character_Controller', function (app) {
                 }]
             });
         },
+        
+        die: function () {
+            // Lock your input
+            if ( this.entity.name === "Player" ) {
+                this.entity.script.Player_Input.lockInput();
+                this.entity.script.Third_Person_Camera.lockInput();
+            }
+
+            // Play death animation
+            var children = this.entity.getChildren();
+
+            children.forEach( function (child) {
+                if ( child.name !== "Camera")
+                    child.enabled = false;
+            });
+
+            // Have him sit there like a dummy
+            // Take him to HELL
+            // Have him solve a problem or 2
+
+            // Respawn him in the world with full health
+            this.entity.rigidbody.enabled = false;
+            this.entity.setPosition( 0, 60, 60 );
+            this.entity.rigidbody.enabled = true;
+            
+            // Control yourself
+            if ( this.entity.name === "Player" ) {
+                this.entity.script.Player_Input.unlockInput();
+                this.entity.script.Third_Person_Camera.unlockInput();
+            }
+
+            // add everythig back
+            children.forEach( function (child) {
+                child.enabled = true;
+            });
+            
+            this.entity.script.Damagable.adjustHealth(50);
+        },
 
         // Move the character in the direction supplied
         move: function (direction) {
--- Damagable.js
+++ Damagable.js
@@ -29,19 +29,32 @@ pc.script.create('Damagable', function (app) {
 
         // This function can either heal the target
         // or deplete its Health Points
-        adjustHealth: function () {
-            this.currentHealth -= 5;
+        adjustHealth: function ( amount ) {
+            this.currentHealth += amount;
 
             if ( this.currentHealth <= 0 ) {
                 this.currentHealth = 0;
-                this.entity.destroy();
+                this.die();
             }
 
             // If this is a Player, update the HTML Graphics thing with current health
             if ( this.entity.name === "Player" )
                 this.User_Interface.setHealthDisplay( this.currentHealth );
-                
+
             console.log("Health remaining: " + this.currentHealth);
+        },
+
+        die : function () {
+            
+            console.log(this.entity.name + " died.");
+
+            // You died...
+            if ( this.entity.name.slice(0, 6) === "Player" ) {
+                // We trust that we have a Character Controller!
+                this.entity.script.Character_Controller.die();
+            } else {
+                this.entity.destroy();
+            }
         }
     };
 
