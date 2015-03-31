3c5bde3524a345731936933504785224ccabcdfa
Yoshgunn <yoshgunn@gmail.com>
2015-03-31 01:20:06 -0400

    Harmful Script
    This is the other half of Damagable.js. While Damagable receives damage,
    Harmful provides it. The guide going forward is this:
    
    - Hazards (spikes, killzones, sawblades) have this script
    - Projectiles have this script
    - Melee weapons (swords, bats, axes) have this script

---
+++ Harmful.js
@@ -0,0 +1,43 @@
+// Harmful
+// - Uses its collision box to hurt whatever it touches
+
+//
+// Requires:
+// - ...
+//
+// Authors: Frank DiCola, Sen Jiang, Slavik Turets
+
+
+pc.script.create('Harmful', function (app) {
+
+    var Harmful = function (entity) {
+        this.entity = entity;
+        this.damage = -200;
+    };
+
+    Harmful.prototype = {
+
+        initialize: function () {
+            console.log("Harmful... Initialized.");
+            this.entity.collision.on('collisionstart', this.onCollisionStart, this);
+        },
+
+        update: function (dt) {
+        },
+
+        onCollisionStart: function (result)
+        {
+            // Find the other's Damagable and adjust it's health
+            if ( result.other.script && result.other.script.Damagable )
+            {
+                result.other.script.Damagable.adjustHealth( this.damage );
+            }
+            else
+                console.log("No Damagable detected");
+
+            this.entity.destroy();
+        }
+    };
+
+    return Harmful;
+});
